d9035dccee60f387268cf92fcfb4f033b73182cc
Karinne App <k.app@students.chapelfield.org>
2019-02-27 12:06:16 -0500

    adds corrects comments that tell what each line of code does

--- arduino/blink/blink.ino
+++ arduino/blink/blink.ino
@@ -8,41 +8,41 @@ void loop() {
   digitalWrite(13, HIGH);
   // sends a signal to the pin telling it to turn on
   delay(1000);
-  // tells pin to stay on for however many milliseconds
+  // tells program to wait for 1000 milliseconds
   digitalWrite(13, LOW);
   // sends a signal to the telling it to turn off
   delay(2000);
-  // tells pin to stay off for however many milliseconds
+  // tells program to wait for 2000 milliseconds
   digitalWrite(13, HIGH);
   // sends a signal to the pin telling it to turn on
   delay(4);
-  // tells pin to stay on for however many milliseconds
+  // tells program to wait for 4 milliseconds
   digitalWrite(13, LOW);
   // sends a signal to the telling it to turn off
   delay(4000);
-  // tells pin to stay off for however many milliseconds
+  // tells program to wait for 4000 milliseconds
   digitalWrite(13, HIGH);
   // sends a signal to the pin telling it to turn on
   delay(9000);
-  // tells pin to stay on for however many milliseconds
+  // tells program to wait for 9000 milliseconds
   digitalWrite(13, LOW);
   // sends a signal to the telling it to turn off
   delay(1000);
-  // tells pin to stay off for however many milliseconds
+  // tells program to wait for 1000 milliseconds
   digitalWrite(13, HIGH);
   // sends a signal to the pin telling it to turn on
   delay(60);
-  // tells pin to stay on for however many milliseconds
+  // tells program to wait 60 milliseconds
   digitalWrite(13, LOW);
   // sends a signal to the telling it to turn off
   delay(3000);
-  // tells pin to stay off for however many milliseconds
+  // tells program to wait for 3000 milliseconds
   digitalWrite(13, HIGH);
   // sends a signal to the pin telling it to turn on
   delay(7000);
-  // tells pin to stay on for however many milliseconds
+  // tells program to wait for 7000 milliseconds
   digitalWrite(13, LOW);
   // sends a signal to the telling it to turn off
   delay(20);
-  // tells pin to stay off for however many milliseconds
+  // tells program to wait for 20 milliseconds
 }
