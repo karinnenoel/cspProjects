a706f138c9be462f38e510918f40ee048b4e2004
Karinne App <k.app@students.chapelfield.org>
2019-02-28 12:01:48 -0500

    Set Serial.begin to 9600 baud, serial.print1n on at digitalWrite high, serial.print1n off at digitalWrite low

--- arduino/serialBlink/serialBlink.ino
+++ arduino/serialBlink/serialBlink.ino
@@ -1,12 +1,15 @@
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(13, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(13, HIGH);
+  Serial.println("On");
   delay(1000);
   digitalWrite(13, LOW);
+  Serial.println("Off");
   delay(1000);
 }
