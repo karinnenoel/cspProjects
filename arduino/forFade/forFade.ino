5e1fed4d4b9cba980a68ea99f94190f599d7d37f
Karinne App <k.app@students.chapelfield.org>
2019-05-16 11:49:51 -0400

    change light value

--- arduino/forFade/forFade.ino
+++ arduino/forFade/forFade.ino
@@ -13,7 +13,9 @@ void loop() {
     delay(10);
   }
 
-  for (int lightValue = 0; lightValue <= 255; lightValue-- ) {
-
+  for (int lightValue = 255; lightValue >= 0; lightValue-- ) {
+    analogWrite(9, lightValue);
+    Serial.println(lightValue);
+    delay(10);
   }
 }
