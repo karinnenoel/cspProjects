70aca96f218ed6139a5c3f9c63cd64f1ebe27a10
Karinne App <k.app@students.chapelfield.org>
2019-06-04 13:21:30 -0400

    Change punctuation.

---
+++ arduino/ifFade/ifFade.ino
@@ -0,0 +1,19 @@
+int lightValue = 0;
+int delta = 1;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(9, OUTPUT);
+
+
+}
+
+void loop() {
+  analogWrite(9, lightValue);
+  Serial.println(lightValue);
+  delay(10);
+  lightValue = lightValue + delta;
+  if (lightValue <= 0 || lightValue >= 255) {
+    delta = -delta;
+  }
+}
